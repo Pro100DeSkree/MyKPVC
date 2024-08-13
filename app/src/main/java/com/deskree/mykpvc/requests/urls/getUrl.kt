14a2d1303a0b01492374f67a22c16ea759180ea6
Андрій/DeSkree <DeSkreeUA@gmail.com>
2024-08-13 22:34:23 +0300

    Added one more condition

--- app/src/main/java/com/deskree/mykpvc/requests/urls/getUrl.kt
+++ app/src/main/java/com/deskree/mykpvc/requests/urls/getUrl.kt
@@ -3,7 +3,8 @@ package com.deskree.mykpvc.requests.urls
 import com.deskree.mykpvc.BuildConfig
 
 fun getUrl(): UrlProvider {
-    if (BuildConfig.IS_DEBUG)
+//    if (BuildConfig.IS_DEBUG)
+    if (false)
         return urlsTest
     else
         return urls
